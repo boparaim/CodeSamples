98dccf28021d44bab79c7de821742765f2553fe9
boparaim <mboparai.singh@gmail.com>
2018-02-15 14:55:27 -0500

    2018.02.15.17.1 Minor: - java selection statements samples update 1

--- src/main/java/com/gurbanivision/mboparai/codesamples/java/SelectionStatements.java
+++ src/main/java/com/gurbanivision/mboparai/codesamples/java/SelectionStatements.java
@@ -11,28 +11,92 @@ package com.gurbanivision.mboparai.codesamples.java;
  */
 public class SelectionStatements {
 
+    private int administrativeCostPercentage = 0;
+    private String searchTerm = "dog";
+    
     public SelectionStatements() {
         
     }
     
+    private int donate(int amount) {
+        System.out.println("donate $"+amount+" to charity A");
+        return amount;
+    }
+    
+    private void showPictures(String relatedTo) {
+        System.out.println("images of "+relatedTo);
+    }
+    
     private void oneWayIfStatement() {
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50);
+        }
         
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50);
     }
     
     private void twoWayIfStatement() {
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50);
+        }
+        else {
+            this.donate(30);
+        }
         
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50);
+        else
+            this.donate(30);
     }
     
     private void multiWayIfStatement() {
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50);
+        }
+        else if (this.administrativeCostPercentage < 10) {
+            this.donate(40);
+        }
+        else {
+            this.donate(30);
+        }
         
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50);
+        else if (this.administrativeCostPercentage < 10)
+            this.donate(40);
+        else
+            this.donate(30);
     }
     
     private void switchStatement() {
+        switch (this.searchTerm) {
+            case "dog":
+                this.showPictures("dog");
+                break;
+            case "cat":
+                this.showPictures("cat");
+                break;
+            default:
+                this.showPictures("scorpion");
+                break;
+        }
         
+        switch (this.searchTerm) {
+            case "dog":
+            case "puppy":
+                this.showPictures("dog");
+                break;
+            default:
+                this.showPictures("scorpion");
+        }
     }
     
     private void conditionalExpressionStatement() {
+        int donatedAmount = (this.administrativeCostPercentage < 5) ? this.donate(50) : this.donate(40);
         
+        donatedAmount = (this.administrativeCostPercentage < 5) ? this.donate(50) 
+                            : (this.administrativeCostPercentage < 10) ? this.donate(40) : this.donate(30);
     }
     
     public static void main(String[] args) {
