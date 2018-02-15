42ae24b128cb789524b74f2c3632b305cdbbc087
boparaim <boparaim.singh@gmail.com>
2018-02-15 14:04:23 -0500

    2018.02.15.17 Major: Initial commit. - java selection statements samples

---
+++ src/main/java/com/gurbanivision/mboparai/codesamples/java/SelectionStatements.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gurbanivision.mboparai.codesamples.java;
+
+/**
+ *
+ * @author root
+ */
+public class SelectionStatements {
+
+    public SelectionStatements() {
+        
+    }
+    
+    private void oneWayIfStatement() {
+        
+    }
+    
+    private void twoWayIfStatement() {
+        
+    }
+    
+    private void multiWayIfStatement() {
+        
+    }
+    
+    private void switchStatement() {
+        
+    }
+    
+    private void conditionalExpressionStatement() {
+        
+    }
+    
+    public static void main(String[] args) {
+        SelectionStatements selectionStatements = new SelectionStatements();
+        selectionStatements.oneWayIfStatement();
+        selectionStatements.twoWayIfStatement();
+        selectionStatements.multiWayIfStatement();
+        selectionStatements.switchStatement();
+        selectionStatements.conditionalExpressionStatement();
+    }
+}
