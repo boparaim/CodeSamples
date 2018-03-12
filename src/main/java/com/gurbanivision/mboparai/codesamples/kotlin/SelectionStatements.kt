0466799234c5ef7becbc82144f4594fa0104fc3a
boparaim <help.gurbaniv@gmail.com>
2018-03-12 10:58:55 -0400

    2018.03.08.17.2 Minor: - kotlin selection statements samples

---
+++ src/main/java/com/gurbanivision/mboparai/codesamples/kotlin/SelectionStatements.kt
@@ -0,0 +1,87 @@
+package com.gurbanivision.mboparai.blog
+
+class SelectionStatements(_administrativeCostPercentage: Int = 0, _searchTerm: String) {
+
+    var administrativeCostPercentage = _administrativeCostPercentage
+    var searchTerm = _searchTerm
+
+    private fun donate(amount: Int): Int {
+        println("donate $$amount to charity A")
+        return amount
+    }
+
+    private fun showPictures(relatedTo: String) {
+        println("images of $relatedTo")
+    }
+
+    fun oneWayIfStatement() {
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50)
+
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50)
+        }
+    }
+
+    fun twoWayIfStatement() {
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50)
+        else
+            this.donate(30)
+
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50)
+        }
+        else {
+            this.donate(30)
+        }
+    }
+
+    fun multiWayIfStatement() {
+        if (this.administrativeCostPercentage < 5)
+            this.donate(50)
+        else if (this.administrativeCostPercentage < 10)
+            this.donate(40)
+        else
+            this.donate(30)
+
+        if (this.administrativeCostPercentage < 5) {
+            this.donate(50)
+        }
+        else if (this.administrativeCostPercentage < 10) {
+            this.donate(40)
+        }
+        else {
+            this.donate(30)
+        }
+    }
+
+    fun whenStatement() {
+        when (this.searchTerm) {
+            "dog" -> {
+                this.showPictures("dog")
+            }
+            "cat" -> {
+                this.showPictures("cat")
+            }
+            else -> {
+                this.showPictures("scorpion")
+            }
+        }
+
+        when (this.searchTerm) {
+            "dog", "puppy" -> this.showPictures("dog")
+            "CAT".toLowerCase() -> this.showPictures("cat")
+            is String -> print("any expression can be used")
+            else -> this.showPictures("scorpion")
+        }
+    }
+
+    fun ifConditionalExpressionStatement() {
+        var donatedAmount = if (this.administrativeCostPercentage < 5) this.donate(50) else this.donate(40)
+        donatedAmount = if (this.administrativeCostPercentage < 5) this.donate(50)
+                        else if (this.administrativeCostPercentage < 10) this.donate(40)
+                        else this.donate(30)
+    }
+
+}
